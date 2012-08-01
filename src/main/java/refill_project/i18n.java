3221f9d61503885a872e798b9fa42ec571351825
Chris Ward <chris@chrisgward.com>
2012-08-01 21:24:31 +1000

    Fixing copyright year

--- src/main/java/refill_project/i18n.java
+++ src/main/java/refill_project/i18n.java
@@ -9,7 +9,7 @@ public class i18n {
         if(key.equals("name"))
             return "RefillBukkit";
         else if(key.equals("copyright"))
-            return String.format("Copyright &copy; %s %s", new SimpleDateFormat("y").format(new Date()), get("name"));
+            return String.format("Copyright &copy; %s %s", new SimpleDateFormat("yyyy").format(new Date()), get("name"));
         return key;
     }
 }
